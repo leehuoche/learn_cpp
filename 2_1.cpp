c3ec51e5c4af7516f5d3e35ecc04b74bafecf7c0
leef <leevs2017@hotmail.com>
2021-06-14 21:39:44 +0800

    cc

--- 2_1.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	cout<<"Hello world"<<endl;
-	cout<<"Welcome to C++"<<endl;
-	return 0;
-}
--- 6_7_2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-	int a[5]={1,2,3,4,5};
-	for(int i=0;i<5;i++)
-		cout<<*(a+i)<<" ";
-	cout<<endl;
-	return 0;
-}
